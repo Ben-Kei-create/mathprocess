e926880264e023429a40874cd81816d3c45b0dbb
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-04-30 21:55:57 +0900

    Initial Commit

---
+++ mathprocess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mathprocess
+//
+//  Created by 茂木史明 on 2026/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
